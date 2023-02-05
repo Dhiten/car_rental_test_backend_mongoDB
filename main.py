ce5a80c917a99cf0d2020ceca02537f7acb02068
Alvaro Cabrera <calvaroj@uninorte.edu.co>
2023-02-04 22:16:32 -0500

    add cors

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from routers.router import include_routes
 from middlewares import add_middlewares
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.title = "car_rental_api"
 app.description = "API for car rental system"
 app.version = "0.0.1"
+
+app.add_middleware(
+     CORSMiddleware,
+     allow_origins=["*"],
+     allow_credentials=True,
+     allow_methods=["*"],
+     allow_headers=["*"],
+)
 
 add_middlewares(app)
 include_routes(app)
